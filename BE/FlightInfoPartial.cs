a5afc3c14ea24b351304a0d02be22d6edec05550
hilaa <hilaa@DESKTOP-IH7ML77>
2022-07-20 17:26:22 +0300

    BE class

---
+++ BE/FlightInfoPartial.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE
+{
+    public class FlightInfoPartial
+    {
+        public int Id { get; set; }
+        public string SourceId { get; set; }
+        public double Long { get; set; }
+        public double Lat { get; set; }
+        public DateTime DateAndTime { get; set; }
+        public string SourceFilter { get; set; }
+        public string Destination { get; set; }
+        public string FlightCode { get; set; }
+    }
+}
