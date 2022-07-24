2c91beba3f88411e92033251d59d000451f8d0bf
hilaa <hilaa@DESKTOP-IH7ML77>
2022-07-24 23:32:04 +0300

    Idal

---
+++ DAL/IDAL.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public interface IDAL
+    {
+        #region FlightInfoPartial
+        void AddFlightInfoPartialToDB(BE.FlightInfoPartial newFlightInfoPartial);
+        BE.FlightInfoPartial GetFlightInfoPartial(Func<BE.FlightInfoPartial, bool> predicate = null);
+
+        void UpdateFlightInfoPartial(BE.FlightInfoPartial updateFlightInfoPartial);
+
+        List<BE.FlightInfoPartial> GetAllFlightInfoPartial(Func<BE.FlightInfoPartial, bool> predicate = null);
+
+        #endregion FlightInfoPartial
+    }
+}
