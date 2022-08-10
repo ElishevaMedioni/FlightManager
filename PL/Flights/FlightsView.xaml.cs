0aaac1c5251042031ed56a766cc07ff968039423
hilaa <hilaa@DESKTOP-IH7ML77>
2022-08-10 16:48:20 +0300

    mvvm

--- PL/Flights/FlightsView.xaml.cs
+++ PL/Flights/FlightsView.xaml.cs
@@ -112,8 +112,6 @@ namespace PL.Flights
         {
             var Flight = flightsViewModel.GetFlightDataViewModel(selected);
 
-            var Flight = flightsViewModel.GetFlightDataViewModel(selected);
-
             DetailsPanel.DataContext = Flight;
 
 
@@ -157,6 +155,67 @@ namespace PL.Flights
                 myMap.Children.Add(PinCurrent);
 
             }
+        }
+
+        void addNewPolyLine(List<BE.Trail> Route)
+        {
+            //new version of MapPolyline -- different fields -- need to adapt
+
+
+            MapPolyline polyline = new MapPolyline();
+            //polyline.Fill = new System.Windows.Media.SolidColorBrush(Colors.Red);
+            polyline.Stroke = new System.Windows.Media.SolidColorBrush(System.Windows.Media.Colors.Red);
+            polyline.StrokeThickness = 1;
+            polyline.Opacity = 0.7;
+            polyline.Locations = new LocationCollection();
+
+            foreach (var item in Route)
+            {
+                polyline.Locations.Add(new Location(item.lat, item.lng));
+            }
+
+            myMap.Children.Clear();
+            myMap.Children.Add(polyline);
+
+
+        }
+
+
+
+
+        private void UpdateWeather(BE.FlightInfoPartial selected)
+        {
+
+            var Flight =flightsViewModel.GetFlightDataViewModel(selected);
+
+            //some of the flight null
+            if (Flight != null)
+            {
+                BE.WeatherRoot weatherRoot = flightsViewModel.GetWeatherWithLatLongViewModel(Flight.airport.destination.position.latitude.ToString(), Flight.airport.destination.position.longitude.ToString());
+                WeatherPanel.DataContext = weatherRoot;
+            }
+        }
+
+        private void dateHoliday()
+        {
+
+            DateTime start = DateTime.Now;
+            bool flag = flightsViewModel.GetHolidayViewModel();
+            if (flag == true)
+            {
+                date.Text = "There is a holiday this week ";
+            }
+            else
+            {
+                date.Text = "There is no holiday this week ";
+
+            }
+        }
+
+        private void myMap_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            ViewAllFlights();
+
         }
     }
 }
