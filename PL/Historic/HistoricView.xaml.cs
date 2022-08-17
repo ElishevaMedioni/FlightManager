e3a2d490fa8586cd65f70c77cdf34ddb032fd785
ElishevaMedioni <medioni@g.jct.ac.il>
2022-08-17 14:26:51 +0300

    change

--- PL/Historic/HistoricView.xaml.cs
+++ PL/Historic/HistoricView.xaml.cs
@@ -43,7 +43,7 @@ namespace PL.Historic
 
         private void DeleteSelectedFlight_Click(object sender, RoutedEventArgs e)
         {
-
+            //FlightsHistoricList.SelectedItem
            // historicViewModel.DeletFlightHistoricViewModel();
         }
 
